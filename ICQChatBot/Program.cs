d26d14d08fdf1391b5439303479707dc5c491355
Антон <live.stone.anton@gmail.com>
2020-05-18 19:21:54 +0500

    Создан эхо-бот

---
+++ ICQChatBot/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using ICQ.Bot;
+using ICQ.Bot.Args;
+
+namespace ICQChatBot
+{
+
+    class Program
+    {
+        private readonly static IICQBotClient bot = new ICQBotClient("[BOT_ID_FROM_ICQ_METABOT]");
+
+        static void Main(string[] args)
+        {
+
+            bot.OnMessage += BotOnMessageReceived;
+            var me = bot.GetMeAsync().Result;
+
+            bot.StartReceiving();
+            Console.WriteLine($"Start listening to @{me.Nick}");
+
+            Console.ReadLine(); //остановка при нажатии Enter
+            bot.StopReceiving();
+        }
+
+        private static void BotOnMessageReceived(object sender, MessageEventArgs messageEventArgs)
+        {
+            var message = messageEventArgs.Message;
+            bot.SendTextMessageAsync(message.From.UserId, message.Text).Wait();
+        }
+    }
+}
